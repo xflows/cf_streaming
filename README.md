ee6b75161f02fe2542fcf39c72e68ce5eb2f2033
Anze Vavpetic <anze.vavpetic@ijs.si>
2017-08-07 10:31:51 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,30 @@
+# ClowdFlows Streaming Module #
+
+
+A [ClowdFlows](https://github.com/xflows/clowdflows/) package, which contains widgets for stream mining. The package can also be used with [ClowdFlows](https://github.com/xflows/clowdflows/) 2.0.
+
+[![Documentation Status](https://readthedocs.org/projects/rdm/badge/?version=latest)](http://clowdflows.readthedocs.io/)
+
+Currently, the project contains components for different corpus operations, basic natural language processing operations such as tokenization, stop word removal, lemmatization, part-of-speech tagging, etc. It also has modules for tweet streaming, term extraction and gender classification.
+
+
+## Installation, documentation ##
+
+```bash
+pip install cf_streaming
+```
+
+Please find other installation instructions, examples and API reference on [Read the Docs](http://clowdflows.readthedocs.io/).
+
+## Note ##
+
+Please note that this is a research project and that drastic changes can be (and are) made pretty regularly. Changes are documented in the [CHANGELOG](CHANGELOG.md).
+
+Pull requests and issues are welcome.
+
+## Contributors to the cf_streaming package code ##
+
+Janez Kranjc (@janezkranjc)
+Anže Vavpetič (@anzev)
+
+* [Knowledge Technologies Department](http://kt.ijs.si), Jožef Stefan Institute, Ljubljana
---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='cf_streaming',
+    version='0.0.1',
+    packages=['cf_streaming'],
+    include_package_data=True,
+    license='MIT License',
+    description='ClowdFlows streaming package',
+    long_description=README,
+    url='https://github.com/xflows/cf_streaming',
+    author='Janez Kranjc, Anze Vavpetic',
+    author_email='janez.kranjc@ijs.si',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        "Django>=1.10.5",
+        "requests",
+    ]
+)
